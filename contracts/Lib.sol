8dfce4588b0c6aa2cead0cf463a1525aafd53a30
"Bao Chen" <"491889203@qq.com">
2022-03-17 13:21:15 +0000

    Alter agreement contract

---
+++ contracts/Lib.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+data usage of actor.
+*/
+struct DataUsage {
+
+    address  actorAddress;
+    address  userAddress;
+    string  serviceName;
+    string servicePurpose;
+    string  operation;
+    string[]  personalData;
+}
+
+/**
+Represent user's vote for data usage by an actor.
+*/
+struct Vote {
+    address userAddress;
+    uint usageID;
+    bool consent;
+}
+
+/**
+Represent the log of processed persoanl data by an actor
+*/
+struct LogContent {
+    address actorAddress; 
+    address userAddress;
+    uint usageID;
+    DataUsage dataUsage;
+}
