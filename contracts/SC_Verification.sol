0f2972c743c26cc86069fbae5e42b52f639e0a15
Bob <491889203@qq.com>
2022-03-18 16:00:27 +0000

    Edit isArrayEqual function in Verification.sol

--- contracts/SC_Verification.sol
+++ contracts/SC_Verification.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "./SC_Agreement.sol";
 import "./SC_DataUsage.sol";
-// import "./Lib.sol";
+import "./Lib.sol";
 
 /**
  * @title Verifier
@@ -15,34 +15,64 @@ contract VerificationContract {
     DataUsageContract private dc;
     AgreementContract private ac;
 
+    // Store all the violators in each 
+
 
     constructor(address dcAdd, address acAdd) {
         dc = DataUsageContract(dcAdd);
         ac = AgreementContract(acAdd);
     }
 
-    
+    // event for EVM logging
+    event Verification(address indexed dataUsageContractAddress,address indexed agreementContractAddress, address violatorAddress);
+    event ViolationDectected(address violator,string message);
 
     function verify(address actorAddress, address userAddress, uint usageID, 
-        string memory serviceName, string memory operation, string[] memory processedData) public returns (uint){
+        string memory serviceName, string memory operation, string[] memory processedData) public returns (address){
 
-        uint violator;
+        address violator;
+        DataUsage memory dataUsage = dc.retrieveDataUsage(usageID);
+        Vote memory vote = ac.retrieveVote(usageID);
 
-
-        if(isStrEqual(dc.retrieveDataUsage(usageID).operation, operation) &&
-            isStrArrayEqual(dc.retrieveDataUsage(usageID).personalData, processedData) &&
-             ac.retrieveVote(usageID).consent == true) {
-                violator = 0;
+        //whether or not the addresses of actors recorded by the log smart contract conform to 
+        //those actors who have been given the consent by the users through the agreement 
+        //smart contract.
+        if(userAddress!=dataUsage.userAddress || userAddress!=vote.userAddress){
+            violator = actorAddress;
+            emit ViolationDectected(violator,"user address inconformity");
+        }   
+        else if(actorAddress!=dataUsage.actorAddress || actorAddress!=vote.actorAddress){
+            violator = actorAddress;
+            emit ViolationDectected(violator,"actor address inconformity");
         }
-        else
-            violator = usageID;
-
+        //whether or not the operations of each actor recorded by the log contract conform to 
+        //those operations which were recorded via the data usage contract and were given the 
+        //users’ consent through the agreement contract.
+        else if(!isStrEqual(dataUsage.operation, operation)){
+            violator = actorAddress;
+            emit ViolationDectected(violator,"operation record inconformity");
+        }
+        //whether or not the processed personal data that were recorded by the log contract 
+        //were already confirmed by the users through the agreement contract.
+        else if(!isStrArrayEqual(processedData, dataUsage.personalData)){
+            violator = actorAddress;
+            emit ViolationDectected(violator,"request processed data inconformity");
+        }
+        //check whether the request above is confirmed by the user subject  
+        else if(!vote.consent){
+            violator = actorAddress;
+            emit ViolationDectected(violator,"data usage is not confirmed by user");
+        }
+       else {
+            violator = address(0x0);
+       }  
+            
+        emit Verification(address(dc), address(ac), violator);
         return violator;
     }
 
 
-
-
+ 
     function isStrEqual(string memory a, string memory b) public pure returns (bool) {
         bytes memory aa = bytes(a);
         bytes memory bb = bytes(b);
@@ -54,19 +84,30 @@ contract VerificationContract {
         return true;
     }
 
-    function isStrArrayEqual(string[] memory a, string[] memory b) public pure returns (bool) {
-        if(a.length == b.length)
-        {
-            for(uint i=0; i<a.length; i++)
-            {
-                if(isStrEqual(a[i], b[i]) == false)
+    function isStrArrayEqual(string[] memory a, string[] memory b) public returns (bool) {
+        if(a.length <= b.length) {
+            bool find;
+            for(uint i=0; i<a.length; i++) {
+                string memory processed = a[i];
+                find = false;
+                for (uint j = 0; j < b.length; j++) {
+                    if(isStrEqual(processed, b[j])) {
+                        find = true;
+                        emit ViolationDectected(address(0), string(abi.encodePacked(processed, " -- ",b[j])));
+                        break;
+                    }
+                } 
+                if (!find) {
+                    emit ViolationDectected(address(0), string(abi.encodePacked(processed, " : can't find!!!")));
                     return false;
+                }
             }
             return true;
+        } else {
+            emit ViolationDectected(address(0), "a.length > b.length");
+            return false;
         }
-        else
- 
-        return false;
+            
     }
 
 }
